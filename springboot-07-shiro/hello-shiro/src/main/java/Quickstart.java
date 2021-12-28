b3b7aee9303a9d7631392c7cf7b47fe17b96a3c7
amzpiper <1131717341@qq.com>
2021-12-28 16:54:22 +0800

    springboot集成shiro

--- springboot-07-shiro/hello-shiro/src/main/java/Quickstart.java
+++ springboot-07-shiro/hello-shiro/src/main/java/Quickstart.java
@@ -35,6 +35,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Quickstart {
 
+    // 日志门面，用来输出日志
     private static final transient Logger log = LoggerFactory.getLogger(Quickstart.class);
 
 
@@ -47,6 +48,7 @@ public class Quickstart {
 
         // Use the shiro.ini file at the root of the classpath
         // (file: and url: prefixes load from files and urls respectively):
+        // 创建SecurityManager
         Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
         SecurityManager securityManager = factory.getInstance();
 
@@ -56,36 +58,51 @@ public class Quickstart {
         // webapps.  That is outside the scope of this simple quickstart, so
         // we'll just do the bare minimum so you can continue to get a feel
         // for things.
+        // 只是在测试时使用，正常不会这么做，使SecurityManager以单例运行。
         SecurityUtils.setSecurityManager(securityManager);
 
         // Now that a simple Shiro environment is set up, let's see what you can do:
+        // 开始了
 
         // get the currently executing user:
+        // Subject出现了，通过这样方式获取当前用户对象Subject。
         Subject currentUser = SecurityUtils.getSubject();
 
         // Do some stuff with a Session (no need for a web or EJB container!!!)
+        // 通过当前用户获取shiro的session
         Session session = currentUser.getSession();
+        // 使用session存数据
         session.setAttribute("someKey", "aValue");
+        // 使用session读取数据
         String value = (String) session.getAttribute("someKey");
         if (value.equals("aValue")) {
             log.info("Retrieved the correct value! [" + value + "]");
         }
 
         // let's login the current user so we can check against roles and permissions:
+        // 判断当前用户是否被认证
         if (!currentUser.isAuthenticated()) {
+            // 如果当前用户被认证，可以通过用户的账号和密码生产token令牌
             UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
+            // 设置记住我
             token.setRememberMe(true);
             try {
+                // ?很恶心的代码
+                // 执行了登录操作
                 currentUser.login(token);
             } catch (UnknownAccountException uae) {
+                // UnknownAccountException用户名不存在
                 log.info("There is no user with username of " + token.getPrincipal());
             } catch (IncorrectCredentialsException ice) {
+                // 密码不错异常
                 log.info("Password for account " + token.getPrincipal() + " was incorrect!");
             } catch (LockedAccountException lae) {
+                // 用户被锁定了
                 log.info("The account for username " + token.getPrincipal() + " is locked.  " +
                         "Please contact your administrator to unlock it.");
             }
             // ... catch more exceptions here (maybe custom ones specific to your application?
+            // 大异常：认证异常
             catch (AuthenticationException ae) {
                 //unexpected condition?  error?
             }
@@ -93,9 +110,11 @@ public class Quickstart {
 
         //say who they are:
         //print their identifying principal (in this case, a username):
+        //获取当前用户的信息
         log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
 
         //test a role:
+        //判断当前用户是否包含某个角色
         if (currentUser.hasRole("schwartz")) {
             log.info("May the Schwartz be with you!");
         } else {
@@ -103,6 +122,7 @@ public class Quickstart {
         }
 
         //test a typed permission (not instance-level)
+        //判断当前用户是否包含某个权限，粗粒度的
         if (currentUser.isPermitted("lightsaber:wield")) {
             log.info("You may use a lightsaber ring.  Use it wisely.");
         } else {
@@ -110,6 +130,7 @@ public class Quickstart {
         }
 
         //a (very powerful) Instance Level permission:
+        //判断当前用户是否包含某个权限，细粒度的
         if (currentUser.isPermitted("winnebago:drive:eagle5")) {
             log.info("You are permitted to 'drive' the winnebago with license plate (id) 'eagle5'.  " +
                     "Here are the keys - have fun!");
@@ -118,6 +139,7 @@ public class Quickstart {
         }
 
         //all done - log out!
+        //注销推出
         currentUser.logout();
 
         System.exit(0);
