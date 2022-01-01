7408227d9baec1e770fef5544414207ed5010abb
amzpiper <1131717341@qq.com>
2022-01-01 14:25:48 +0800

    配置Swagger3的扫描接口

--- swagger2-demo/src/main/java/com/kuang/Swagger2DemoApplication.java
+++ swagger2-demo/src/main/java/com/kuang/Swagger2DemoApplication.java
@@ -3,6 +3,9 @@ package com.kuang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 郭宇航
+ */
 @SpringBootApplication
 public class Swagger2DemoApplication {
 
